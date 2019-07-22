f1abf79ce8a5638f2f85fcd66c9976fc00f19275
zhuzhengpeng <53170725+zhuzhengpeng@users.noreply.github.com>
2019-07-22 18:14:29 +0800

    同步

--- app/src/main/java/com/example/chronus/MainActivity.java
+++ app/src/main/java/com/example/chronus/MainActivity.java
@@ -1,12 +1,40 @@
 package com.example.chronus;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.nav_calendar:
+
+                    return true;
+                case R.id.nav_reminders:
+
+                    return true;
+                case R.id.nav_timeline:
+
+                    return true;
+                case R.id.nav_tomato:
+
+                    return true;
+                case R.id.nav_settings:
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +42,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button btnOK = (Button) findViewById(R.id.button);
         btnOK.setOnClickListener(this);
-
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
     @Override
     public void onClick(View view){
         TextView text = (TextView) findViewById(R.id.textView);
-        text.setText("我爱萱萱");
+        text.setText("Hello");
     }
 }
